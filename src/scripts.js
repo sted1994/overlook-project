953654c953c511a203361aab59e2db50b20e77c3
Stephen Tedesco <s_tylert@icloud.com>
2022-04-19 17:13:15 -0600

    Link files

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,8 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-
+import { getData } from './api-calls.js'
+import './images/Lotus-Flower.svg'
+//import './images/overlook.jpeg'
 console.log('This is the JavaScript entry file - your code begins here.');
+getData('bookings')
